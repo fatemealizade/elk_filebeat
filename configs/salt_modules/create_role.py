45e3753853388093a5d1bb3d41928934df782767
fatemealizade <alizade.fateme78@gmail.com>
2022-12-16 16:41:43 +0330

    Update modules

--- configs/salt_modules/create_role.py
+++ configs/salt_modules/create_role.py
@@ -1,20 +1,70 @@
-import json
-import salt
 import requests
-import jinja2
+import json
+
 
 def request():
-    for user in __pillar__['users']:
-      url = "http://192.168.33.21:9200/_security/user/"+user['username']
-      payload = json.dumps({
-        "password": user['password'],
-        "roles": user['roles'],
-        "full_name": user['full_name'],
-        "email": user['email'],
-      })
-      headers = {
-        'Authorization': 'Basic ZWxhc3RpYzpBYTEyMzQ1Ng==',
-        'Content-Type': 'application/json'
-      }
-      
-      response = requests.request("POST", url, headers=headers, data=payload)
+   
+   for role in __pillar__['roles']:
+     url = "http://192.168.33.21:5601/api/security/role/"+role['rolename']
+     if ( role['rolename'] == 'admin' or role['rolename'] == 'NOC'):
+        payload = json.dumps({
+            "elasticsearch": {
+              "cluster" : role['elastic_privileges'],
+              "indices" : [{
+                  "names": role['indices'],
+                  "privileges": role['privileges']
+              }]
+            },
+            "kibana": [
+              {
+                "base": ["all"],
+                "feature": {
+                },
+                "spaces": [
+                  "default"
+                ]
+              }
+            ]
+        })
+     else:
+        payload = json.dumps({
+          "elasticsearch": {
+            "cluster": role['elastic_privileges'],
+            "indices": [
+              {
+               "names": role['indices'],
+               "privileges": role['privileges']
+              }
+            ]
+          },
+          "kibana": [
+            {
+              "base": [],
+              "feature": {
+                "discover": [role['discover_access']],
+                "visualize": [role['visualize_access']],
+                "dashboard": [role['dashboard_access']],
+                "dev_tools": [role['dev_tools_access']],
+                "advancedSettings": [role['advanceSettings_access']],
+                "indexPatterns": [role['indexPatterns_access']],
+                "graph": [role['graph_access']],
+                "apm": [role['apm_access']],
+                "maps": [role['maps_access']],
+                "canvas": [role['canvas_access']],
+                "infrastructure": [role['infrastructure_access']],
+                "logs": [role['logs_access']],
+                "uptime": [role['uptime_access']]
+                },
+              "spaces": [
+                "default"
+              ]
+            }
+          ]
+        })
+     headers = {
+       'kbn-xsrf': 'true',
+       'Authorization': 'Basic ZWxhc3RpYzpBYTEyMzQ1Ng==',
+       'Content-Type': 'application/json'
+     }
+     
+     response = requests.request("PUT", url, headers=headers, data=payload)
